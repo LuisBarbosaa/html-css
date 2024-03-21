56bb45cb4630b549552b44b6815bed522261e1af
LuisBarbosaa <luisbarbosaa1212@gmail.com>
2024-03-21 12:37:04 -0300

    Initial commit

---
+++ trabalho/trote.unifacef/calculadora.js
@@ -0,0 +1,5 @@
+function pontos() {
+    let sangue = Number (document.getElementById("sangue").value)
+    let kits = Number (document.getElementById("kits").value)
+    let resultado = 20 * sangue + 
+}
